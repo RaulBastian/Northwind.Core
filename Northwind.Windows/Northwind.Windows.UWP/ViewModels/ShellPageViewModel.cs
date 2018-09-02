bf201cbf8c7dbbe91da2c4db6db02b9c225b5377
Raul <raulbastian@outlook.com>
2018-09-02 17:55:35 +0200

    Set up UWP with NavigationView (not Xamarin) and PRISM

---
+++ Northwind.Windows/Northwind.Windows.UWP/ViewModels/ShellPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Northwind.Windows.UWP.ViewModels {
+    public class ShellPageViewModel : BindableBase {
+
+
+        private List<NavigationViewItem> navigationViewItem = new List<NavigationViewItem>(new NavigationViewItem[] 
+        {
+            new NavigationViewItem() {
+                Content = "Customers",
+                DataContext = "Customers"
+            },
+            new NavigationViewItem() {
+                Content = "Products",
+                DataContext = "Products"
+            }
+        });
+
+        public ShellPageViewModel() {
+
+        }
+
+        public List<NavigationViewItem> NavigationViewItems {
+            get {
+                return navigationViewItem;
+            }
+        }
+
+    }
+}
--- Northwind.Windows/Northwind.Windows.UWP/Views/ShellPage.xaml.cs
+++ Northwind.Windows/Northwind.Windows.UWP/Views/ShellPage.xaml.cs
@@ -13,18 +13,20 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Northwind.Windows.UWP
-{
+namespace Northwind.Windows.UWP.Views {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class ShellPage : Page {
+        public ShellPage() {
             this.InitializeComponent();
+            this.rootFrame.Navigated += RootFrame_Navigated;
+        }
+
+        private void RootFrame_Navigated(object sender, NavigationEventArgs e) {
+            
         }
     }
 }
